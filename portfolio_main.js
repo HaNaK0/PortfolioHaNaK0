3306c83cfff38d9272c798dc9f472dbdf3cb36d3
HaNaK0 <saturn-v@hotmail.com>
2014-12-11 00:22:05 +0100

    changed around

--- portfolio_main.js
+++ portfolio_main.js
@@ -1,6 +1,45 @@
+//var mouseOnAbFlag = false
+
 $(document).ready(function() {
-	$('.navButton').mouseenter(function() {
-		$('div').animate({left: '200px'})
-		/*$('this').animate({opacity: 1.0})*/
+	//animation for about button
+	$('.navButton#about').mouseenter(function() {
+		$(this).css({
+		'width': '200',
+		"transition-property": "width, color",
+		"transition-duration": "2s",
+		"transition-delay": "2s, 0",
+	
+		"-webkit-transition-property": "width, color",
+		"-webkit-transition-duration": "2s",
+		"-webkit-transition-delay": "2s, 0",
+	
+		"-moz-transition-property": "width, color",
+		-moz-transition-duration: 2s;
+		-moz-transition-delay: 0, 2s;
+		
+		});
 	});
+	
+	$('.navButton#about').mouseleave(function() {
+		$(this).css('width', '100');
+		$(this).css('color', 'rgba(255,255,255,0)');
+	}); 
+	
+	//animation for game button
+	$('.navButton#games').mouseenter(function() {
+		$(this).css('width', '200');
+	});
+	
+	$('.navButton#games').mouseleave(function() {
+		$(this).css('width', '100');
+	}); 
+	
+	//animation for contact button 
+	$('.navButton#contact').mouseenter(function() {
+		$(this).css('width', '200');
+	});
+	
+	$('.navButton#contact').mouseleave(function() {
+		$(this).css('width', '100');
+	}); 
 });
