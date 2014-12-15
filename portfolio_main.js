cac205e4b64fb2c5c1be7b69a89f3d5d1e3370c7
HaNaK0 <saturn-v@hotmail.com>
2014-12-16 00:28:39 +0100

    sliding panes
    did the sliding panels javascript

--- portfolio_main.js
+++ portfolio_main.js
@@ -1,45 +1,58 @@
 //var mouseOnAbFlag = false
 
 $(document).ready(function() {
-	//animation for about button
-	$('.navButton#about').mouseenter(function() {
-		$(this).css({
-		'width': '200',
-		"transition-property": "width, color",
-		"transition-duration": "2s",
-		"transition-delay": "2s, 0",
+	//animation for all buttons
+	var $currentPane = $('.contentPane#about')
 	
-		"-webkit-transition-property": "width, color",
-		"-webkit-transition-duration": "2s",
-		"-webkit-transition-delay": "2s, 0",
-	
-		"-moz-transition-property": "width, color",
-		-moz-transition-duration: 2s;
-		-moz-transition-delay: 0, 2s;
+	$('.navButton').mouseenter(function() {
+		$(this).css('transition-delay', '0, .5s');
+		$(this).css('-moz-transition-delay', '0, .5s');
+		$(this).css('-webkit-transition-delay', '0, .5s');
 		
-		});
+		$(this).css('width', '300');
+		$(this).css('color', 'rgba(255,255,255,1)');
 	});
 	
-	$('.navButton#about').mouseleave(function() {
-		$(this).css('width', '100');
+	$('.navButton').mouseleave(function() {
+		$(this).css('transition-delay', '.5s, 0');
+		$(this).css('-moz-transition-delay', '.5s, 0');
+		$(this).css('-webkit-transition-delay', '.5s, 0');
+		
 		$(this).css('color', 'rgba(255,255,255,0)');
-	}); 
-	
-	//animation for game button
-	$('.navButton#games').mouseenter(function() {
-		$(this).css('width', '200');
+		$(this).css('width', '100');
 	});
 	
-	$('.navButton#games').mouseleave(function() {
-		$(this).css('width', '100');
-	}); 
 	
-	//animation for contact button 
-	$('.navButton#contact').mouseenter(function() {
-		$(this).css('width', '200');
+	//if about button is clicked
+	$('.navButton#about').click(function() {
+		$currentPane.css('z-index', '1')
+		$currentPane.css('left', '-100%')
+		
+		$currentPane = $('.contentPane#about')
+		
+		$currentPane.css('z-index', '2')
+		$currentPane.css('left', '0%')
 	});
 	
-	$('.navButton#contact').mouseleave(function() {
-		$(this).css('width', '100');
-	}); 
+	//if game button is clicked
+	$('.navButton#games').click(function() {
+		$currentPane.css('z-index', '1')
+		$currentPane.css('left', '-100%')
+		
+		$currentPane = $('.contentPane#games')
+		
+		$currentPane.css('z-index', '2')
+		$currentPane.css('left', '0%')
+	});
+	
+	//if contact button is clicked
+	$('.navButton#contact').click(function() {
+		$currentPane.css('z-index', '1')
+		$currentPane.css('left', '-100%')
+		
+		$currentPane = $('.contentPane#contact')
+		
+		$currentPane.css('z-index', '2')
+		$currentPane.css('left', '0%')
+	});
 });
